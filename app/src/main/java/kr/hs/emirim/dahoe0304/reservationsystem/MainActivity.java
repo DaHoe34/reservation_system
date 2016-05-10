797af5df4841bfba1631f090744c25f79036358d
Dahoe Shin <kity990304@gmail.com>
2016-05-10 21:02:14 +0900

    #4 Third Event Programming

--- app/src/main/java/kr/hs/emirim/dahoe0304/reservationsystem/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dahoe0304/reservationsystem/MainActivity.java
@@ -1,5 +1,7 @@
 package kr.hs.emirim.dahoe0304.reservationsystem;
 
+import android.graphics.Color;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,12 +12,14 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
     Chronometer chrono;
     Button butStart, butDone;
     RadioButton radioDate, radioTime;
     CalendarView calView;
-    TimePicker timePicker;
+    TimePicker timePick;
     TextView textResult;
 
     @Override
@@ -29,9 +33,9 @@ public class MainActivity extends AppCompatActivity {
         radioDate=(RadioButton)findViewById(R.id.radio_date);
         radioTime=(RadioButton)findViewById(R.id.radio_time);
         calView=(CalendarView)findViewById(R.id.calendar);
-        timePicker=(TimePicker)findViewById(R.id.time_pick);
+        timePick=(TimePicker)findViewById(R.id.time_pick);
         textResult=(TextView)findViewById(R.id.text_result);
-        timePicker.setVisibility(View.INVISIBLE);
+        timePick.setVisibility(View.INVISIBLE);
         calView.setVisibility(View.INVISIBLE);  //화면에 두가지 뷰가 안보임. 가려진다.
 
 
@@ -39,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 calView.setVisibility(View.VISIBLE);    //보이게
-                timePicker.setVisibility(View.INVISIBLE);   //안보이게
+                timePick.setVisibility(View.INVISIBLE);   //안보이게
 
 
             }
@@ -48,9 +52,34 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 calView.setVisibility(View.INVISIBLE);    //안보이게
-                timePicker.setVisibility(View.VISIBLE);   //보이게
+                timePick.setVisibility(View.VISIBLE);   //보이게
 
+            }
+        });
 
+        butStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chrono.setBase(SystemClock.elapsedRealtime());
+                chrono.start();
+                chrono.setTextColor(Color.RED);
+            }
+        });
+        butDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chrono.stop();
+                chrono.setTextColor(Color.BLUE);
+                Calendar cal=Calendar.getInstance();
+                cal.setTimeInMillis(calView.getDate());
+                int year=cal.get(Calendar.YEAR);    //연도를 반환받을 수있다.
+                int month=cal.get(Calendar.MONTH)+1;    //왜 +1이면 month은 0월달부터 시작이여서
+                int date=cal.get(Calendar.DATE);
+                /*int hour=timePicker.getHour();
+                int minute=timePick*/
+
+                String dateAndTime=year+"년 "+month+"월 "+date+"일 ";
+                textResult.setText(dateAndTime);
             }
         });
     }
