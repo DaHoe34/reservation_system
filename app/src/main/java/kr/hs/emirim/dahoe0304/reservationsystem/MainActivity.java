67db99f3289964a88a11778c5af878157423bb06
Dahoe Shin <kity990304@gmail.com>
2016-05-10 19:32:12 +0900

    #2 First Event Programming

--- app/src/main/java/kr/hs/emirim/dahoe0304/reservationsystem/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dahoe0304/reservationsystem/MainActivity.java
@@ -2,12 +2,37 @@ package kr.hs.emirim.dahoe0304.reservationsystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+    Chronometer chrono;
+    Button butStart, butDone;
+    RadioButton radioDate, radioTime;
+    CalendarView calView;
+    TimePicker timePicker;
+    TextView textResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chrono=(Chronometer)findViewById(R.id.chrono);    //강제형변환을 해줘야됨. 이게 반환하는건 view이다.
+        butStart=(Button)findViewById(R.id.but_start);
+        butDone=(Button)findViewById(R.id.but_done);
+        radioDate=(RadioButton)findViewById(R.id.radio_date);
+        radioTime=(RadioButton)findViewById(R.id.radio_time);
+        calView=(CalendarView)findViewById(R.id.calendar);
+        timePicker=(TimePicker)findViewById(R.id.time_pick);
+        textResult=(TextView)findViewById(R.id.text_result);
+        timePicker.setVisibility(View.INVISIBLE);
+        calView.setVisibility(View.INVISIBLE);  //화면에 두가지 뷰가 안보임. 가려진다.
+
     }
 }
